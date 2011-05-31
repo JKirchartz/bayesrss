87d787bec862b87ae0d02e2589dbdbf18353164b
tomdaffurn <tomdaffurn@gmail.com>
2011-05-31 13:13:14 +0000

    itemstore is multi-feed friendly

--- bayesrss/itemstore.py
+++ bayesrss/itemstore.py
@@ -5,6 +5,12 @@ from email.utils import parsedate
 
 from bayesrss.models import *
 
+class FeedInfo:
+    def __init__(self, items, fetchtime):
+        self.items = items
+        self.fetchtime = fetchtime
+        self.itemstore = {}
+        
 class ItemStore:
     def __init__(self, hitCounter, classifier):
         """The last time the itemstore was rebuilt"""
@@ -14,35 +20,34 @@ class ItemStore:
         self.feedstore = dict()
         
         """A map of item to an ItemClassification for that item"""
-        #TODO make this multi-feed friendly
-        self.itemstore = dict()
         self.hitCounter = hitCounter
         self.classifier = classifier
         
     def getDictionary(self, feedkey):
-        items, fetchtime = self.get_items(feedkey)
-        logging.info("buildtime="+str(self.buildtime) + " fetchtime="+str(fetchtime))
+        feed_info = self.get_items(feedkey)
+        logging.info("buildtime="+str(self.buildtime) + " fetchtime="+str(feed_info.fetchtime))
         
-        if self.buildtime is None or self.buildtime < fetchtime:
+        if self.buildtime is None or self.buildtime < feed_info.fetchtime:
             logging.info("Rebuilding item dictionary")
             self.buildtime = datetime.now()
-            for key, value in self.itemstore.items():
+            for key, value in feed_info.itemstore.items():
                 if value.isStale():
-                    del self.itemstore[key]
+                    del feed_info.itemstore[key]
             classifier = self.classifier()
-            for it in items:
-                if not self.itemstore.has_key(it.hash()):
-                    self.itemstore[it.hash()] = ItemClassification(it, classifier.spamprob(it.getTokens()), it.pub_datetime)
+            for it in feed_info.items:
+                if not feed_info.itemstore.has_key(it.hash()):
+                    feed_info.itemstore[it.hash()] = ItemClassification(it, classifier.spamprob(it.getTokens()), it.pub_datetime)
         else:
             logging.info("Returned prebuilt item dictionary")
-        return self.itemstore
+        return feed_info.itemstore
     
     def getItem(self, key):
         return self.itemstore[key]
             
-    def get_new_items(self, url):
+    def get_new_items(self, key):
+        feed = Feed.get(key)
         self.hitCounter.countFetchFeedHit()
-        xml = urllib2.urlopen(url).read()
+        xml = urllib2.urlopen(feed.link).read()
         items = []
         tree = etree.fromstring(xml)
         for node in tree.find("channel").findall("item"):
@@ -57,16 +62,19 @@ class ItemStore:
     
     def get_items(self, key):
         if self.feedstore.has_key(key):
-            logging.info("Feed was found in feedstore")
-            items, fetchTime = self.feedstore[key]
-            if self.isRecent(fetchTime):
-                logging.info("Returning items from cache")
-                return self.feedstore[key]
-        feed = Feed.get(key)
-        items = self.get_new_items(feed.link)
-        self.feedstore[key] = items, datetime.now()
-        logging.info("Fetched new items")
-        return items, datetime.now()      
+            logging.info("get_items: Feed was found in feedstore")
+            feed_info = self.feedstore[key]
+            if self.isRecent(feed_info.fetchtime):
+                logging.info("get_items: Returning items from cache")
+            else:
+                logging.info("get_items: Fetched new items")
+                feed_info.items = self.get_new_items(key)
+                feed_info.fetchtime = datetime.now()
+            return feed_info
+        feed_item = FeedInfo(self.get_new_items(key), datetime.now())
+        self.feedstore[key] = feed_item
+        logging.info("get_items: created new FeedInfo")
+        return feed_item  
         
     def isRecent(self, time):
         return (time is not None 
