ab4f862fd54fe1e2860f6d8abdf3742d6dcd0395
tomdaffurn <tomdaffurn@gmail.com>
2011-06-14 07:09:55 +0000

    add some safewords for seek; new version for dev

--- bayesrss/safewords.py
+++ bayesrss/safewords.py
@@ -4,4 +4,4 @@ safewords = set(["the", "of", "to", "and", "a", "in", "is", "it", "you", "that",
     "their", "time", "if", "will", "way", "about", "many", "then", "them", "write", "would", "like", "so", "these", "her", "long",
     "make", "thing", "see", "him", "two", "has", "look", "more", "day", "could", "go", "come", "did", "number", "sound", "no", 
     "most", "people", "my", "over", "know", "water", "than", "call", "first", "who", "may", "down", "side", "been", "now", "find",
-"any"])
+"any", "developer", "software", "engineer", "permanent", "brisbane", "analyst"])
